600a529ef8bde2d87d858dffcfec2e372cb5cc5b
Igor Baesse de Araujo <igorbaesse@timcoo.com.br>
2018-09-29 22:38:46 -0300

    finished project config

--- bin/server.js
+++ bin/server.js
@@ -1,24 +1,14 @@
 "use strict";
+const app =require('../src/app')
 const http = require("http");
 const debug = require("debug")("nodestr:srver");
-const express = require("express");
 
-const app = express();
 const port = normalizePort(process.env.PORT || '3000');
 app.set("port", port);
-
 const server = http.createServer(app);
-const router = express.Router();
-
-let route = router.get("/", (req, res, next) => {
-  res.status(200).send({
-    title: "bem vindo",
-    version: "1.0.0"
-  });
-});
-app.use("/", route);
 server.listen(port);
-server.on('error',onError);
+server.on('error', onError);
+server.on('listening', onListening)
 console.log('runing......');
 
 function normalizePort(val) {
@@ -53,4 +43,12 @@ function onError(error) {
     default:
       throw error;
   }
+}
+
+function onListening() {
+  const addr = server.address();
+  const bind = typeof addr === 'string'
+    ? 'pipe ' + addr
+    : 'port ' + addr.port
+  debug('Listening on ' + bind);
 }
